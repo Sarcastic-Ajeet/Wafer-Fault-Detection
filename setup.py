3abfbd93a6f0f097cd462a1fda92226f6c5805e2
Sarcastic <ajeetkumar905864@gmail.com>
2024-09-10 03:34:51 +0530

    Fisrt Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Wafer Fault Detection",
+    version="0.0.1",
+    author="Sarcastic",
+    author_email="example@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ src/constant/__init__.py
@@ -0,0 +1,6 @@
+# AWS_S3_BUCKET_NAME = ""
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "wafer"
+MONGO_COLLECTION_NAME = "wafer_detail"
+MONGO_DB_URL="mongodb+srv://ajeetkumar1:ail2FNU2naTQ3zle@cluster0.rhi9ggj.mongodb.net/"
+# mongodb+srv://Ajeetkumar:wNq8H0uJ1aNGYUKpcluster0.3m1eycf.mongodb.net/
